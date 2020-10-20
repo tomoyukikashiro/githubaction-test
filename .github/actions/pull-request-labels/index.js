9829a1206261f46c01e36295b6989218e9d658b2
tomoyukikashiro <ksrtmyk@gmail.com>
2020-10-20 09:57:40 +0900

    feat(label): to an array

--- .github/actions/pull-request-labels/index.js
+++ .github/actions/pull-request-labels/index.js
@@ -2,60 +2,72 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 
 const uniq = array => [...new Set(array)];
-
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const LABEL_TO_DELETE = core.getInput('label_to_delete')
-const LABEL_TO_ADD = core.getInput('label_to_add')
-const eventData = github.context.payload;
-
-core.info(LABEL_TO_DELETE)
-core.info(LABEL_TO_ADD)
-
-if (eventData.action !== 'submitted' || eventData.review.state !== 'approved') {
-  core.info('[Finish] PR have not been approved.');
-  return;
+const toLabelsArray = labelsKey => {
+  const labelsStr = core.getInput(labelsKey);
+  if (!labelsStr) throw new Error(`${labelsKey} is required.`);
+  return labelsStr.split(',').map(label => label.trim());
 }
 
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const eventData = github.context.payload;
+
+const isApprovalFlow = eventData.action === 'submitted' && eventData.review.state === 'approved';
+const isRequestChangeFlow = eventData.action === 'submitted' && eventData.review.state === 'changes_requested';
 const [ owner, repo ] = eventData.repository.full_name.split('/');
 const { number: pullNumber } = eventData.pull_request;
+const octokit = github.getOctokit(GITHUB_TOKEN);
 
+const replaceLabels = async (labels, toDelete, toAdd) => {
+  const newLabels = labels
+    .map(label => label.name)
+    .filter(label => !toDelete.includes(label))
+    .concat(toAdd);
+  await octokit.issues.setLabels({
+    owner,
+    repo,
+    issue_number: pullNumber,
+    labels: newLabels
+  })
+}
+
+const approvalFlow = async (reviews, labels) => {
+  const labelsToDelete = toLabelsArray('approval_labels_to_delete');
+  const labelsToAdd = toLabelsArray('approval_labels_to_add');
+  const uniqueReviewerIds = uniq(reviews.map(review => review.user.id));
+  const approvedReviews = uniqueReviewerIds
+    .filter(userId => !!reviews.find(review => review.user.id === userId && review.state === 'APPROVED'));
+
+  if (uniqueReviewerIds.length !== approvedReviews.length) {
+    core.info('[FINISH] All reviewers have not approved yet.');
+    return;
+  }
+  await replaceLabels(labels, labelsToDelete, labelsToAdd);
+}
+
+const requestChangeFlow = async (reviews, labels) => {
+  const labelsToDelete = toLabelsArray('request_change_labels_to_delete');
+  const labelsToAdd = toLabelsArray('request_change_labels_to_add');
+  await replaceLabels(labels, labelsToDelete, labelsToAdd);
+}
+
+// main
 (async () => {
+  const { data: reviews } = await octokit.pulls.listReviews({
+    owner,
+    repo,
+    pull_number: pullNumber,
+  });
+  const { data: labels } = await octokit.issues.listLabelsOnIssue({owner, repo, issue_number: pullNumber})
   try {
-    const octokit = github.getOctokit(GITHUB_TOKEN);
-
-    const { data: reviews } = await octokit.pulls.listReviews({
-      owner,
-      repo,
-      pull_number: pullNumber,
-    });
-
-    const uniqueReviewerIds = uniq(reviews.map(review => review.user.id));
-    const approvedReviews = uniqueReviewerIds
-      .filter(userId => !!reviews.find(review => review.user.id === userId && review.state === 'APPROVED'));
-
-    if (uniqueReviewerIds.length !== approvedReviews.length) {
-      core.info('[Finish] All reviewers have not approved yet.');
-      return;
+    if (isApprovalFlow) {
+      core.info('[START] add label(s) for approval.');
+      await approvalFlow(reviews, labels);
+    } else if (isRequestChangeFlow) {
+      core.info('[START] add labels(s) for request changes.');
+      await requestChangeFlow(reviews, labels);
+    } else {
+      core.info('[SKIP] PR state have not changed for labeling.');
     }
-
-    await octokit.issues.addLabels({
-      owner,
-      repo,
-      issue_number: pullNumber,
-      labels: [LABEL_TO_ADD]
-    });
-
-    core.info('add label')
-
-    await octokit.issues.removeLabel({
-      owner,
-      repo,
-      issue_number: pullNumber,
-      name: LABEL_TO_DELETE
-    });
-    core.info('remove label')
-    core.info('[Finish] All reviewers have just approved.');
-
   } catch (e) {
     core.setFailed(`[Error] ${e}`);
   }
